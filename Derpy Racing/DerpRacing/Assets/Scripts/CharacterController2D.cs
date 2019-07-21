b735e5fc6bb54b4159ef95a67cb68d0800cb957e
Sven Zobler <svenzobler@gmail.com>
2019-07-21 17:58:45 -0400

    Initial level blockout, level ideas mechanics etc

---
+++ Derpy Racing/DerpRacing/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 9;
+    [SerializeField]
+    float walkAcceleration = 75;
+    [SerializeField]
+    float airAcceleration = 30;
+    [SerializeField]
+    float groundDeceleration = 70;
+    [SerializeField]
+    float jumpHeight = 4;
+    private BoxCollider2D boxCollider;
+    private Vector2 velocity;
+    private bool grounded;
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        //jump input
+        if (grounded)
+        {
+            velocity.y = 0;
+            if (Input.GetButtonDown("Jump"))
+                velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+
+
+        }
+
+        //gravity
+        velocity.y += Physics2D.gravity.y * Time.deltaTime;
+        float acceleration = grounded ? walkAcceleration : airAcceleration;
+        float deceleration = grounded ? groundDeceleration : 0;
+        //joystick for movement
+        float buttons = Input.GetAxisRaw("Horizontal");
+
+        //side to side movement
+        if (buttons != 0)
+            velocity.x = Mathf.MoveTowards(velocity.x, speed * buttons, acceleration * Time.deltaTime);
+        else
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+
+        transform.Translate(velocity * Time.deltaTime);
+
+        //collision detection
+        Collider2D[] hit = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
+
+        grounded = false;
+        foreach (Collider2D hits in hit)
+        {
+            if (hits == boxCollider)
+                continue;
+
+            ColliderDistance2D colDistance = hits.Distance(boxCollider);
+
+            if (colDistance.isOverlapped)
+            {
+                if (Vector2.Angle(colDistance.normal, Vector2.up) < 90 && velocity.y < 0)
+                {
+                    grounded = true;
+                }
+                transform.Translate(colDistance.pointA - colDistance.pointB);
+            }
+        }
+
+    }
+}
---
+++ Derpy Racing/DerpRacing/Assets/Scripts/camControl.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camControl : MonoBehaviour
+{
+    public List<Transform> targets;
+
+    public Vector3 offset;
+    public float smooth = .5f;
+    private Vector3 velocity;
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+
+    public float zoomLimiter = 50f;
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+    private void FixedUpdate()
+    {
+
+        if (targets.Count == 0)
+            return;
+
+        Move();
+        Zoom();
+       
+    }
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+    void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smooth);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+
+        }
+        return bounds.size.x;
+    }
+    Vector3 GetCenterPoint()
+    {
+        if(targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+
+        }
+        return bounds.center;
+    }
+}
+
