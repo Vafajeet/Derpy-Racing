841ec67d0422d9479a3295c4134e068b7545a7fb
YaBoiRayden <Tradderley@gmail.com>
2019-07-23 10:58:10 -0400

    stuff

--- Derpy Racing/Assets/Scripts/ItemSystem.cs
+++ Derpy Racing/Assets/Scripts/ItemSystem.cs
@@ -8,51 +8,35 @@ public class ItemSystem : MonoBehaviour
     [SerializeField] GameObject[] Items;
     [SerializeField] int curIndex;
     [SerializeField] Transform[] spawnLocal;
-    [SerializeField] bool pressed;
-    [SerializeField] float delay;
-    [SerializeField] float curCooldown;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<CharacterController2D>();
+
+        SpawnItem();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q) == true)
-        {
-            pressed = true;
-        }
 
+    }
+    void FixedUpdate()
+    {
         curIndex = Random.Range(0, 3);
-
-        if (curCooldown < delay)
-        {
-            curCooldown += Time.deltaTime;
-        }
-        if(curCooldown >= delay)
-        {
-            curCooldown = delay;
-            if (pressed == true)
-            {
-                SpawnItem();               
-            }
-        }
     }
 
     void SpawnItem()
     {
-        curCooldown = 0;
-        Instantiate(Items[curIndex], spawnLocal[curIndex]);
-        pressed = false;
+        Instantiate(Items[curIndex], spawnLocal[0]);
     }
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if(other.gameObject == player.gameObject)
+        if (spawnLocal[0].gameObject == player.gameObject)
         {
-            Instantiate(Items[curIndex], spawnLocal[curIndex]);
+            Debug.Log("Touched");
+            Instantiate(Items[curIndex], player.transform);
         }
     }
 }
