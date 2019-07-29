4908314784205c915ac482782bf3ada0cd8809b6
jesserivera257 <33045560+jesserivera257@users.noreply.github.com>
2019-07-29 09:47:58 -0400

    My Items
    My level i created
    My prefabs of items
    Level creation tools (Ground & Platform)
    Textures that are needed for my level creation

---
+++ Assets/Scripts/PowerupHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupHandler : MonoBehaviour
+{
+   // public GameObject pickupEffect;
+
+    public float multiplier = 1.4f;
+    public float duration = 4f;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine (PickUp(other));
+        }
+    }
+
+    IEnumerator PickUp(Collider2D player)
+    {
+        Debug.Log("I pick it up");
+       // Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        CharacterController2D stats = player.GetComponent<CharacterController2D>();
+        stats.speed *= multiplier;
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        yield return new WaitForSeconds(duration);
+        stats.speed /= multiplier;
+
+        Destroy(gameObject);
+    }
+}
